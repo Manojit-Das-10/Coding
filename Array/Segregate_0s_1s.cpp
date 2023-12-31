a2d3bdd2bea48eacf7274e4f2b38b93fbe23e5c9
Manojit Das <manojitdas180306@gmail.com>
2023-12-31 23:05:36 +0530

    Solving GFG problem

---
+++ Array/Segregate_0s_1s.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    void segregate0and1(int arr[],int n)
+    {
+    sort(arr,arr+n); 
+    }
+};
+
+int main()
+{
+
+
+return 0;
+}
