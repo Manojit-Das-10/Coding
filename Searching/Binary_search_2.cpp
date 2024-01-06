536d91916f498a6aeb7a7b2b1f0ff271f5ad5e81
Manojit Das <manojitdas180306@gmail.com>
2024-01-06 15:14:53 +0530

    Adding Binary search using vector

---
+++ Searching/Binary_search_2.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int expontial(vector<int>arr,int x){
+    int s=0;
+    int e=arr.size()-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==x)
+        {
+            return 1;
+        }
+        else if (arr[mid]>x)
+        {
+            e=mid-1;
+            
+        }
+        else
+        {
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return 0;
+
+}
+
+int main(){
+    vector<int>arr{3,4,5,6,11,13,15,33,56,70};
+    int x= 33;
+    // int ans=expontial(arr,x);
+    if(expontial(arr,x)==1)
+    {
+        cout<<"True"<<endl;
+    }
+    else
+    {
+        cout<<"False";
+    }
+    
+}
