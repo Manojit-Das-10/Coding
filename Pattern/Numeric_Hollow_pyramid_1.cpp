60e5ece5d679ca6088acde54c21116bd70465805
Manojit Das <manojitdas180306@gmail.com>
2024-01-13 01:26:31 +0530

    Adding new pattern

---
+++ Pattern/Numeric_Hollow_pyramid_1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+int n;
+cout<<"Range:";
+cin>>n;
+for (int i = 1; i < n; i++)
+{
+    for (int j = 1; j < i+2; j++)
+    {
+        cout<<j<<" ";
+    }
+    cout<<endl;
+    
+}
+
+
+return 0;
+}
