e9bed9a87a713eae7cef187df9ef218484c8da09
Manojit Das <manojitdas180306@gmail.com>
2024-01-12 11:27:55 +0530

    Adding palindrome problem solutions

---
+++ Leetcode/Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int original = x;
+        long  reversed = 0;
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+};
