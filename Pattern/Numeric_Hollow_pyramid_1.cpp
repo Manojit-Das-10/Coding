eb69a6e4a79a8eef9f6f76bc19fc533707ae0397
Manojit Das <manojitdas180306@gmail.com>
2024-01-13 19:59:49 +0530

    Updating the code

--- Pattern/Numeric_Hollow_pyramid_1.cpp
+++ Pattern/Numeric_Hollow_pyramid_1.cpp
@@ -1,22 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int main()
 {
-int n;
-cout<<"Range:";
-cin>>n;
-for (int i = 1; i < n; i++)
-{
-    for (int j = 1; j < i+2; j++)
+    int n;
+    cout << "Range:";
+    cin >> n;
+    for (int i = 1; i < n; i++)
     {
-        cout<<j<<" ";
+        for (int j = 1; j < i + 2; j++)
+        {
+            
+            if(j==1||j==i+1||i==n-1)
+            {
+                cout << j<<" ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout << endl;
     }
-    cout<<endl;
-    
-}
 
-
-return 0;
+    return 0;
 }
