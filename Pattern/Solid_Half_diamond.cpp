cbe95b099393b1ef8c8b627a20a1817503eecfd8
Manojit Das <manojitdas180306@gmail.com>
2024-01-25 00:12:48 +0530

    Updating

--- Pattern/Solid_Half_diamond.cpp
+++ Pattern/Solid_Half_diamond.cpp
@@ -9,21 +9,20 @@ cout<<"Enter the range:";
 cin>>n;
 for (int i = 0; i < (n*2)-1; i++)
 {
-    int cond =0;
-    if(i<n)
-    {
-        cond=i;
-    }
-    else{
-        cond= (n-(i%n)-2);
-    }
-    {
+    int cond = i<n?i:n-(i%n)-2;
+    // int cond =0;
+    // if(i<n)
+    // {
+    //     cond=i;
+    // }
+    // else{
+    //     cond= (n-(i%n)-2);
+    // }
     for (int j = 0; j <= cond; j++)
         {
             cout<<" * ";
         } 
     cout<<endl;
-    }
 }
 return 0;
 }
