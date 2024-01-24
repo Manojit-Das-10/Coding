4a7f4261921005829244792e3b4ff7171a6fc933
Manojit Das <manojitdas180306@gmail.com>
2024-01-25 00:03:31 +0530

    Adding a new pattern

---
+++ Pattern/Solid_Half_diamond.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+int n;
+cout<<"Enter the range:";
+cin>>n;
+for (int i = 0; i < (n*2)-1; i++)
+{
+    int cond =0;
+    if(i<n)
+    {
+        cond=i;
+    }
+    else{
+        cond= (n-(i%n)-2);
+    }
+    {
+    for (int j = 0; j <= cond; j++)
+        {
+            cout<<" * ";
+        } 
+    cout<<endl;
+    }
+}
+return 0;
+}
