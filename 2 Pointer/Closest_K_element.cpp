44fcfadaeb615802aac566e82bedaee825628d26
Manojit Das <manojitdas180306@gmail.com>
2024-02-21 21:44:23 +0530

    Solving 3 closest number

---
+++ 2 Pointer/Closest_K_element.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l=0, h=arr.size()-1;
+        while(h-l>=k)
+        {
+            if(x-arr[l] > arr[h]-x)
+            {
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        vector<int>ans;
+        for(int i=l ; i<=h; i++)
+        {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 3;
+    int x = 4;
+
+    vector<int> result = solution.findClosestElements(arr, k, x);
+
+    cout << "Closest " << k << " elements to " << x << " are: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+
+return 0;
+}
