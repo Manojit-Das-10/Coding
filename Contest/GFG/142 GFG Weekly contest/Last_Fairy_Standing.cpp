fe401f795967efca3b1e1ae972fd9fb4f5100ce8
Manojit Das <manojitdas180306@gmail.com>
2024-03-09 19:57:24 +0530

    Updating

--- Contest/GFG/142 GFG Weekly contest/Last_Fairy_Standing.cpp
+++ Contest/GFG/142 GFG Weekly contest/Last_Fairy_Standing.cpp
@@ -4,7 +4,13 @@ using namespace std;
 class Solution {
   public:
     int lastFairyStanding(int n, int k) {
-        // code here
+  /*
+        int result = 0;
+        for (int i = 2; i <= n; ++i) {
+            result = (result + k) % i;
+        }
+        return result + 1;
+  */
         k = k-1;
         int idx = k;
         vector<int>v;
