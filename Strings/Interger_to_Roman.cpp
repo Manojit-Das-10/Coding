e5aabd98a1687ccbe2c9b6e5173e496b16370aed
Manojit Das <manojitdas180306@gmail.com>
2024-04-16 22:33:12 +0530

    solving string problem

---
+++ Strings/Interger_to_Roman.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        string romanSymbols[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        string ans = "";
+
+        for (int i = 0; i < 13; i++)
+        {
+           while (num >= values[i])
+           {
+                ans += romanSymbols[i];
+                num -= values[i];
+           }
+           
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+Solution sol;
+int num;
+cout<<"Enter the integer:";
+cin>>num;
+string ans = sol.intToRoman(num);
+cout<<"The romanic form is:"<<ans<<endl;
+return 0;
+}
