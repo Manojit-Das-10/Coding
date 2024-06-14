a3e321f461533692fc9841ad8c199a4a5b1149df
Manojit Das <manojitdas180306@gmail.com>
2024-06-14 21:36:20 +0530

    simple basic coding

---
+++ Basic/test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+int i=5;
+
+cout<<++i<<endl;
+cout<<i--<<endl;
+cout<<--i<<endl;
+cout<<i++<<endl;
+
+
+int a = 5;
+int b = ++a + a++ + a--;
+cout<<b<<endl;
+
+return 0;
+}
