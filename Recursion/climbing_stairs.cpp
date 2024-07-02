4185b3b03090b2b19de2aa55a4f05e405f1b5277
Manojit Das <manojitdas180306@gmail.com>
2024-07-02 11:28:48 +0530

    solving the leetcode problem

---
+++ Recursion/climbing_stairs.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int a=1, b=2, ans;
+        for(int i=3;i<=n;i++){
+            ans=a+b;
+            a=b;
+            b=ans;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+Solution sol;
+int n;
+cout<<"Enter the number of strirs:";
+cin>>n;
+
+int ans = sol.climbStairs(n);
+cout<<"The number of ways is:"<<ans<<endl;
+
+return 0;
+}
