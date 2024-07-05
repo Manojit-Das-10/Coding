bd5971dd39272edf1d74239dcf5758bd429ee2a2
Manojit Das <manojitdas180306@gmail.com>
2024-07-05 10:36:35 +0530

    SOlving the coin chain problem

---
+++ Recursion/Coin_chain_problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&arr , int target){
+    // Base Case
+    if(target == 0){
+        return 0;
+    }
+    if(target < 0){
+        return INT_MAX;
+    }
+    // 1 case solve karo
+    int mini = INT_MAX;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int ans = solve(arr,target - arr[i]);
+        if(ans != INT_MAX){
+            mini = min(mini , ans+1);
+        }
+    }
+    
+    return mini;
+}
+
+int main()
+{
+vector<int>arr{1,2};
+int target = 5;
+
+int ans = solve(arr,target);
+cout<<"The answer is: "<<ans<<endl;
+
+return 0;
+}
