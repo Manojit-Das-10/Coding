b254e77d9dff3b30b08d87e1a08b4dbec3f43da4
Manojit Das <manojitdas180306@gmail.com>
2024-07-31 19:30:22 +0530

    N queen

--- Recursion/N_Queen.cpp
+++ Recursion/N_Queen.cpp
@@ -1,92 +1,101 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printSolution(vector<vector<char>>&board,int n,int count){
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout<<board[i][j]<<" ";
+unordered_map<int,bool>leftRowCheck;
+unordered_map<int,bool>upperLeftDiagonalCheck;
+unordered_map<int,bool>bottomLeftDiagonalCheck;
+
+
+
+void printSolution(vector<vector<char>>& board, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << board[i][j] << " ";
         }
-        cout<<endl;
-        
+        cout << endl;
     }
-    count++;
-    cout<<endl<<endl;    
+    cout << endl << endl;
 }
 
-bool isSafe(int row,int col,vector<vector<char>>&board,int n){
-    // check for left row
-    int i=row;
-    int j=col;
-    while (j>=0)
-    {
-        if(board[i][j] == 'Q'){
-            return false;
-        }
-        j--;
-    }
+bool isSafe(int row, int col, vector<vector<char>>& board, int n) {
 
-    // check for upper left diagonal
-    i=row;
-    j=col;
-    while (i>=0 && j>=0)
-    {
-        if(board[i][j] == 'Q'){
-            return false;
-        }
-        i--;
-        j--;
+    if(leftRowCheck[row] == true|| upperLeftDiagonalCheck[n-1+(col-row)] == true || bottomLeftDiagonalCheck[col+row] == true){
+        return false;
     }
+    
+    // Check for left row
+    // int i = row;
+    // int j = col;
+    // while (j >= 0) {
+    //     if (board[i][j] == 'Q') {
+    //         return false;
+    //     }
+    //     j--;
+    // }
 
-    // check for lower left diagonal
-     i=row;
-     j=col;
-    while (i<n && j>=0)
-    {
-        if(board[i][j] == 'Q'){
-            return false;
-        }
-        i++;
-        j--;
-    }
+    // Check for upper left diagonal
+    // i = row;
+    // j = col;
+    // while (i >= 0 && j >= 0) {
+    //     if (board[i][j] == 'Q') {
+    //         return false;
+    //     }
+    //     i--;
+    //     j--;
+    // }
+
+    // Check for lower left diagonal
+    // i = row;
+    // j = col;
+    // while (i < n && j >= 0) {
+    //     if (board[i][j] == 'Q') {
+    //         return false;
+    //     }
+    //     i++;
+    //     j--;
+    // }
 
     return true;
-    
 }
 
-void solve(vector<vector<char>>&board,int col,int n){
+void solve(vector<vector<char>>& board, int col, int n) {
     // Base case
-    int count = 0;
-    if(col >= n){
-        printSolution(board,n,count);
+    if (col >= n) {
+        printSolution(board, n);
         return;
     }
 
-    // solve one case
-    for (int row = 0; row < n; row++)
-    {
-        if(isSafe(row,col,board,n)){
-            board[row][col]='Q';
+    // Solve one case
+    for (int row = 0; row < n; row++) {
+        if (isSafe(row, col, board, n)) {
+            // Placed Queen
+            board[row][col] = 'Q';
+
+            // Insert the element in the map
+            leftRowCheck[row] = true;
+            upperLeftDiagonalCheck[n-1+(col-row)] = true;
+            bottomLeftDiagonalCheck[col+row] = true;
+
+
             // Recursion
-            solve(board,col+1 , n);
+            solve(board, col + 1, n);
 
             // Backtracking
             board[row][col] = '-';
+            leftRowCheck[row] = false;
+            upperLeftDiagonalCheck[n-1+(col-row)] = false;
+            bottomLeftDiagonalCheck[col+row] = false;
         }
-
     }
-    
 }
 
 int main() {
-int n;
-cout<<"Enter board size:";
-cin>>n;
+    int n;
+    cout << "Enter board size: ";
+    cin >> n;
 
-vector<vector<char>> board(n, vector<char>(n, '-'));
-int col=0;
+    vector<vector<char>> board(n, vector<char>(n, '-'));
+    solve(board, 0, n);
 
-solve(board,col,n);
-return 0;
+    return 0;
 }
---
+++ Recursion/n-Queen_Leetcode.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    unordered_map<int,bool>leftRowCheck;
+    unordered_map<int,bool>upperLeftDiagonalCheck;
+    unordered_map<int,bool>bottomLeftDiagonalCheck;
+
+    void storeSolution(vector<vector<char>>& board, int n , vector<vector<string>>&ans) {
+        vector<string>temp;
+        for (int i = 0; i < n; i++) {
+            string store="";
+            for (int j = 0; j < n; j++) {
+                store.push_back(board[i][j]);
+            }
+            temp.push_back(store);
+        }
+        ans.push_back(temp);
+    }
+
+
+    bool isSafe(int row, int col, vector<vector<char>>& board, int n) {
+
+        if(leftRowCheck[row] == true|| upperLeftDiagonalCheck[n-1+(col-row)] == true || bottomLeftDiagonalCheck[col+row] == true){
+            return false;
+        }
+
+        return true;
+    }
+
+    void solve(vector<vector<char>>& board, int col, int n , vector<vector<string>>&ans) {
+        // Base case
+        if (col >= n) {
+            storeSolution(board, n, ans);
+            return;
+        }
+
+        // Solve one case
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                // Placed Queen
+                board[row][col] = 'Q';
+
+                // Insert the element in the map
+                leftRowCheck[row] = true;
+                upperLeftDiagonalCheck[n-1+(col-row)] = true;
+                bottomLeftDiagonalCheck[col+row] = true;
+
+
+                // Recursion
+                solve(board, col + 1, n,ans);
+
+                // Backtracking
+                board[row][col] = '.';
+                leftRowCheck[row] = false;
+                upperLeftDiagonalCheck[n-1+(col-row)] = false;
+                bottomLeftDiagonalCheck[col+row] = false;
+            }
+        }
+    }
+
+
+
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        solve(board, 0, n , ans);
+
+        return ans;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the board size (n): ";
+    cin >> n;
+
+    Solution sol;
+    vector<vector<string>> solutions = sol.solveNQueens(n);
+
+    cout << "Total number of solutions: " << solutions.size() << endl;
+    for (const auto& solution : solutions) {
+        for (const auto& row : solution) {
+            cout << row << endl;
+        }
+        cout << endl;
+    }
+
+return 0;
+}
