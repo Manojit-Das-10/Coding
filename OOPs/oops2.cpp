61e0e56d004e8086573ff3c7c6605a0589311daf
Manojit Das <manojitdas180306@gmail.com>
2024-09-23 00:28:26 +0530

    learning opps 2 lec

---
+++ OOPs/oops2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Animal {
+
+    public:
+        int age;
+        int weight;
+        
+
+        void eat(){
+            cout<<"Eating"<<endl;
+        }
+
+};
+
+class Dog : public Animal{
+
+};
+
+int main() {
+
+Dog d1;
+d1.eat();
+
+return 0;
+}
